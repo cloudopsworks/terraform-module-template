84802011bb6cb1a3685f27643cf28d89dcaaa9c1
Cristian Beraha <cristian@cloudops.works>
2024-08-14 12:20:25 -0300

    chore: update template for future use

--- locals.tf
+++ locals.tf
@@ -9,4 +9,6 @@ locals {
   system_name       = format("%s-%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def, local.region)
   system_name_short = format("%s-%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(var.org.environment_type), 0, 4), var.spoke_def, local.region)
   all_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
+  secret_store_path = format("/%s/%s/%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
+
 }
