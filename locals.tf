936150ffaa8951cd515467079ca4e4866423d0d5
Cristian Beraha <cristian@cloudops.works>
2025-06-26 15:19:53 -0300

    Upgrade AWS provider to v5 and modify region variable to use `id` instead of `name`.

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@
 #            Distributed Under Apache v2.0 License
 #
 locals {
-  region_arr        = split("-", data.aws_region.current.name)
+  region_arr        = split("-", data.aws_region.current.id)
   region            = format("%s%s%s", lower(local.region_arr[0]), lower(substr(local.region_arr[1], 0, 2)), lower(local.region_arr[2]))
   system_name       = format("%s-%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def, local.region)
   system_name_short = format("%s-%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def, local.region)
