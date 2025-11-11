e6a2a091282de16f005938974f9db871e9a8a038
Cristian Beraha <cristian@cloudops.works>
2025-11-11 10:17:03 -0300

    PIP-8 chore: update tag generation logic in locals.tf

--- .cloudopsworks/gcp/locals.tf
+++ .cloudopsworks/gcp/locals.tf
@@ -13,7 +13,8 @@ locals {
   system_name_short = format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
   system_name_sub30 = format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 1), substr(lower(var.org.environment_name), 0, 1), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
   system_name_env   = format("%s-%s", substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
-  all_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
+  std_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
+  all_tags          = {for k, v in local.std_tags: lower(k) => replace(lower(v), "/[ //@#$+=.]/", "_")}
   secret_store_path = format("/%s/%s/%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
 
 }
