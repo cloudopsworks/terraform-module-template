6e73411665335b5d100a4548458c30f3d6aaa3cc
Cristian Beraha <cristian@cloudops.works>
2026-01-28 11:13:29 -0300

    chore: update variable descriptions and defaults in `variables.tf`

--- variables.tf
+++ variables.tf
@@ -6,13 +6,15 @@
 #       WebSite: https://cloudops.works
 #     Distributed Under Apache v2.0 License
 #
-# Establish this is a HUB or spoke configuration
+
+# is_hub: false # (Optional) Is this a hub or spoke configuration? Default: false
 variable "is_hub" {
   description = "Is this a hub or spoke configuration?"
   type        = bool
   default     = false
 }
 
+# spoke_def: "001" # (Optional) Spoke ID Number, must be a 3 digit number. Default: "001"
 variable "spoke_def" {
   description = "Spoke ID Number, must be a 3 digit number"
   type        = string
@@ -23,6 +25,11 @@ variable "spoke_def" {
   }
 }
 
+# org: # (Required) Organization details
+#   organization_name: "example" # (Required) The name of the organization
+#   organization_unit: "devops"  # (Required) The unit within the organization
+#   environment_type: "production" # (Required) The type of environment (e.g. production, staging)
+#   environment_name: "prod"      # (Required) The name of the environment
 variable "org" {
   description = "Organization details"
   type = object({
@@ -33,6 +40,8 @@ variable "org" {
   })
 }
 
+# extra_tags: # (Optional) Extra tags to add to the resources. Default: {}
+#   Tag1: "Value1"
 variable "extra_tags" {
   description = "Extra tags to add to the resources"
   type        = map(string)
