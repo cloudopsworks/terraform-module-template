51ade2b18e5dacd458fcd3f7c2fc16c642fdfae3
Cristian Beraha <cristian@cloudops.works>
2026-03-03 13:19:09 -0300

    chore: add Azure boilerplate with region mapping and update GCP locals for region support

---
+++ .cloudopsworks/boilerplate/azurerm/locals.tf
@@ -0,0 +1,74 @@
+##
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
+#
+
+locals {
+  region_map = {
+    asiapacific        = "apac"
+    australiacentral   = "auce"
+    australiacentral2  = "auce2"
+    australiaeast      = "auea"
+    australiasoutheast = "ause"
+    austriaeast        = "atea"
+    belgiumcentral     = "bece"
+    belgiumwest        = "bewe"
+    brazilsouth        = "brs"
+    brazilsoutheast    = "brse"
+    canadacentral      = "cace"
+    canadaeast         = "caea"
+    centralindia       = "cein"
+    centralus          = "cus"
+    chilecentral       = "clce"
+    eastasia           = "ea"
+    eastus             = "eus"
+    eastus2            = "eus2"
+    francecentral      = "frce"
+    francesouth        = "frso"
+    germanynorth       = "deno"
+    germanywestcentral = "dewc"
+    indonesiacentral   = "idce"
+    israelcentral      = "ilce"
+    italynorth         = "itno"
+    japaneast          = "jpea"
+    japanwest          = "jpwe"
+    koreacentral       = "krce"
+    koreasouth         = "krso"
+    malaysiawest       = "mywe"
+    mexicocentral      = "mxce"
+    newzealandnorth    = "nzno"
+    northcentralus     = "ncus"
+    norwayeast         = "nwea"
+    norwaywest         = "nwwe"
+    polandcentral      = "plce"
+    qatarcentral       = "qace"
+    southafricanorth   = "sano"
+    southafricawest    = "sawo"
+    southcentralus     = "scus"
+    southeastasia      = "sea"
+    southindia         = "sind"
+    spaincentral       = "spce"
+    swedencentral      = "sece"
+    switzerlandnorth   = "chno"
+    switzerlandwest    = "chwe"
+    taiwan             = "tw"
+    uaecentral         = "uaec"
+    uaenorth           = "uaen"
+    uksouth            = "uks"
+    ukwest             = "ukw"
+    westcentralus      = "wcus"
+    westindia          = "wind"
+    westus             = "wus"
+    westus2            = "wus2"
+    westus3            = "wus3"
+  }
+  short_region      = lookup(local.region_map, var.region, "")
+  system_name       = local.short_region != "" ? format("%s-%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def, local.short_region) : format("%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def)
+  system_name_short = local.short_region != "" ? format("%s-%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def, local.short_region) : format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
+  all_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
+  secret_store_path = format("/%s/%s/%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
+}
--- .cloudopsworks/boilerplate/gcp/locals.tf
+++ .cloudopsworks/boilerplate/gcp/locals.tf
@@ -8,14 +8,13 @@
 #
 
 locals {
-  # region_arr        = split("-", data.google_project.current.id)
-  # region            = format("%s%s%s", lower(local.region_arr[0]), lower(substr(local.region_arr[1], 0, 2)), lower(local.region_arr[2]))
-  system_name       = format("%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def)
-  system_name_short = format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
-  system_name_sub30 = format("%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 1), substr(lower(var.org.environment_name), 0, 1), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
-  system_name_env   = format("%s-%s", substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def)
+  region_arr        = split("-", data.google_client_config.current.region)
+  region            = format("%s%s%s", lower(local.region_arr[0]), lower(substr(local.region_arr[1], 0, 2)), lower(substr(local.region_arr[1], -1, 1)))
+  system_name       = format("%s-%s-%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type), var.spoke_def, local.region)
+  system_name_short = format("%s-%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 3), substr(lower(var.org.environment_name), 0, 3), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def, local.region)
+  system_name_sub30 = format("%s-%s-%s-%s-%s", substr(lower(var.org.organization_unit), 0, 1), substr(lower(var.org.environment_name), 0, 1), substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def, local.region)
+  system_name_env   = format("%s-%s-%s", substr(lower(replace(var.org.environment_type, "-", "")), 0, 4), var.spoke_def, local.region)
   std_tags          = merge(module.tags.locals.common_tags, var.extra_tags)
-  all_tags          = {for k, v in local.std_tags: lower(k) => replace(lower(v), "/[ //@#$+=.]/", "_")}
+  all_tags          = { for k, v in local.std_tags : lower(k) => replace(lower(v), "/[ //@#$+=.]/", "_") }
   secret_store_path = format("/%s/%s/%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
-
 }
